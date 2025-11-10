9888e9887245fa4a4eaa626d888c69a9c3637867
Mike Judge <mike@substantial.com>
2025-11-09 17:19:58 -0800

    Add a sample mcp server

--- README.md
+++ README.md
@@ -6,3 +6,29 @@ Just messing around in typescript learning @modelcontextprotocol/sdk
 * Get the right version of nodejs (optional): `asdf install`
 * Install packages: `npm install`
 
+## Running it
+
+`npm run start`
+
+## Inspecting it
+
+`npx @modelcontextprotocol/inspector`
+connect to the streamable HTTP URL http://localhost:3000/mcp
+
+## Adding it to cursor
+
+1. Go to settings
+2. Select "Tools & MCP" in sidebar
+3. Select "New MCP Server"
+4. In mcp.json (which opens) do something like this:
+
+```
+{
+  "mcpServers": {
+    "dice": {
+      "url": "http://localhost:3000/mcp",
+      "headers": {}
+    }
+  }
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,61 @@
-const hello = (): void => {
-  console.log("Hello from typescript");
-};
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+import express from "express";
+import { z } from "zod";
 
-hello();
+// Create an mcp server
+const server = new McpServer({
+  name: "demo-server",
+  version: "1.0.0",
+});
+
+server.registerTool(
+  "roll-dice",
+  {
+    title: "Dice Rolling Tool",
+    description: "Rolls dice and returns the result",
+    inputSchema: { diceSides: z.number() },
+    outputSchema: { rollResult: z.number() },
+  },
+  async ({ diceSides }) => {
+    const output = {
+      rollResult: Math.ceil(Math.random() * diceSides),
+    };
+
+    return {
+      content: [{ type: "text", text: JSON.stringify(output) }],
+      structuredContent: output,
+    };
+  }
+);
+
+const app = express();
+app.use(express.json());
+
+app.post("/mcp", async (req, res) => {
+  // Create a new transport for each request to prevent request ID collisions
+  const transport = new StreamableHTTPServerTransport({
+    sessionIdGenerator: undefined,
+    enableJsonResponse: true,
+  });
+
+  res.on("close", () => {
+    transport.close();
+  });
+
+  await server.connect(transport);
+  await transport.handleRequest(req, res, req.body);
+});
+
+const port = parseInt(process.env.PORT || "3000");
+app
+  .listen(port, () => {
+    console.log(`Demo MCP Server running on http://localhost:${port}/mcp`);
+  })
+  .on("error", (error) => {
+    console.error("Server error:", error);
+    process.exit(1);
+  });
